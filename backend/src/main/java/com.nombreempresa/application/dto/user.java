e34c6cbbef0f16fa71616a42f30d0ca91d27ae93
Mario Molano <mariormolano@yahoo.com>
2024-07-13 12:00:17 -0500

    Directo Organización carpetas

---
+++ backend/src/main/java/com.nombreempresa/application/dto/user.java
@@ -0,0 +1,10 @@
+abstract class user {
+
+  int id;
+  String username;
+  String email;
+  String password;
+  String role;
+  String createdAt;
+  String updatedAt;
+}
---
+++ frontend/src/logic/controllers/dto/user.ts
@@ -0,0 +1,9 @@
+abstract class user{
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    role: string;
+    createdAt: string;
+    updatedAt: string;
+}
